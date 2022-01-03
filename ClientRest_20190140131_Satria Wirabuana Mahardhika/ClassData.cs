1411d55a34355fc18f9d05a0fb4511baa7baa336
swmahardhika <swmahardhika@gmail.com>
2022-01-03 22:32:43 +0700

    Add project files.

---
+++ ClientRest_20190140131_Satria Wirabuana Mahardhika/ClassData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+namespace ClientRest_20190140131_Satria_Wirabuana_Mahardhika
+{
+    class ClassData
+    {
+        public void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+        
+            foreach(var mhs in data)
+            {
+                Console.WriteLine("NIM: " + mhs.nim);
+                Console.WriteLine("Nama: " + mhs.nama);
+                Console.WriteLine("Prodi: " + mhs.prodi);
+                Console.WriteLine("Angkatan: " + mhs.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClientRest_20190140131_Satria Wirabuana Mahardhika/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ClientRest_20190140131_Satria Wirabuana Mahardhika")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClientRest_20190140131_Satria Wirabuana Mahardhika")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("340eb0d0-26e0-45b6-b1cb-2baab4d6cf11")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
